60a8b702894721cb457ecc70cfcd3479b1c5f840
Ed <egonzgar00@gmail.com>
2025-09-23 21:44:13 +0200

    se agrega libros

--- src/Libro.java
+++ src/Libro.java
@@ -1,4 +1,15 @@
-public record Libro(String titulo, String autor, int anio, boolean disponible) {
-    public Libro prestar() { return new Libro(titulo, autor, anio, false); }
-    public Libro devolver() { return new Libro(titulo, autor, anio, true); }
+public record Libro(String titulo, String autor, int anio, EstadoLibro estado) {
+    public Libro(String titulo, String autor, int anio) {
+        this(titulo, autor, anio, EstadoLibro.DISPONIBLE);
+    }
+
+    public Libro prestar() {
+        return new Libro(titulo, autor, anio, EstadoLibro.PRESTADO);
+    }
+
+    public Libro devolver() {
+        return new Libro(titulo, autor, anio, EstadoLibro.DISPONIBLE);
+    }
 }
+
+enum EstadoLibro { DISPONIBLE, PRESTADO, RESERVADO }
