489f6bd1840bbe76664b6d207e2551c43357f9fd
Ed <egonzgar00@gmail.com>
2025-09-24 21:16:21 +0200

    se agrega main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        ConsolaBiblioteca app = new ConsolaBiblioteca();
+        IBibliotecaService biblioteca = new Biblioteca();
+        ConsolaBiblioteca app = new ConsolaBiblioteca(biblioteca);
         app.iniciar();
     }
 }
