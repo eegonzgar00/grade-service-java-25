d450255fedb39ba59ada07cdcc438a9635b530cb
Ed <egonzgar00@gmail.com>
2025-09-28 18:10:02 +0200

    se agregan metodos

--- src/IBibliotecaService.java
+++ src/IBibliotecaService.java
@@ -1,10 +1,11 @@
 import java.util.List;
+import java.util.UUID;
 
 public interface IBibliotecaService {
-
     void registrarLibro(String titulo, String autor, int anio);
-    List<Libro> buscar(String texto);
-    boolean prestar(int indice);
-    boolean devolver(int indice);
     List<Libro> getLibros();
+    List<Libro> buscar(String texto);
+    boolean prestar(UUID id);
+    boolean devolver(UUID id);
+    boolean reservar(UUID id);
 }
