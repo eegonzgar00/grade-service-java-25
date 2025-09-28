9c97af0dfe896164b86528735d7451c89828d9fa
Ed <egonzgar00@gmail.com>
2025-09-28 18:09:19 +0200

    cambios main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        IBibliotecaService biblioteca = new Biblioteca();
+        IBibliotecaService biblioteca = new BibliotecaServiceImpl();
         SwingUtilities.invokeLater(() -> {
             new BibliotecaFrame(biblioteca).setVisible(true);
         });
