2e1ade0dfa0f06ad55bbd7b53ecd9c923109b377
Ed <egonzgar00@gmail.com>
2025-10-06 22:17:06 +0200

    se agrega clase para run de spring

---
+++ src/BibliotecaWebApp.java
@@ -0,0 +1,11 @@
+package com.learn.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaWebApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaWebApp.class, args);
+    }
+}
