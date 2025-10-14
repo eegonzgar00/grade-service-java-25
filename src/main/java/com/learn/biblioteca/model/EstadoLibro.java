7d2c980086eb67a9bf0ef7729b68a9e6a9f22197
Ed <egonzgar00@gmail.com>
2025-10-14 23:04:53 +0200

    cambios estado libro

--- src/main/java/com/learn/biblioteca/model/EstadoLibro.java
+++ src/main/java/com/learn/biblioteca/model/EstadoLibro.java
@@ -1,17 +1,6 @@
 package com.learn.biblioteca.model;
 
-sealed interface EstadoLibro permits Disponible, Prestado, Reservado {
+public sealed interface EstadoLibro permits Disponible, Prestado, Reservado {
     String nombre();
 }
 
-record Disponible() implements EstadoLibro {
-    @Override public String nombre() { return "✅ Disponible"; }
-}
-
-record Prestado() implements EstadoLibro {
-    @Override public String nombre() { return "📕 Prestado"; }
-}
-
-record Reservado() implements EstadoLibro {
-    @Override public String nombre() { return "📌 Reservado"; }
-}
